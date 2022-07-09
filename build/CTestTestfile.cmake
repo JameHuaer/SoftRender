472833869b0531fd42306ed6cad01287a89fcb0e
江达明 <jiangdaming@zulong.com>
2022-07-09 09:16:04 +0800

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Learning/MyRender/SoftRender
-# Build directory: E:/Learning/MyRender/SoftRender/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
